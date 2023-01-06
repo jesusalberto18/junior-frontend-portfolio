50a2c1fbf677c965d6f9620fc895d5746bb268fb
Alberto <albertiae.digital@gmail.com>
2023-01-05 20:09:02 -0400

    Created the script for scroll reveal

---
+++ js/scroll.js
@@ -0,0 +1,13 @@
+ScrollReveal().reveal('.start');
+
+ScrollReveal().reveal('.btn-cv');
+
+ScrollReveal().reveal('.about', { delay: 1000 });
+
+ScrollReveal().reveal('.projects', { delay: 1000 });
+
+ScrollReveal().reveal('.skills', { delay: 1000 });
+
+ScrollReveal().reveal('.faq', { delay: 1000 });
+
+ScrollReveal().reveal('.contact', { delay: 1000 });
