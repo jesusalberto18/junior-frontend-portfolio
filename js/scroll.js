934c17e514df72ff98de89ec99c0aad1f108ebc5
Alberto <albertiae.digital@gmail.com>
2023-01-06 14:28:35 -0400

    Changed the classes to ids for the scroll reveal effect

--- js/scroll.js
+++ js/scroll.js
@@ -6,14 +6,14 @@ ScrollReveal().reveal('.start');
 
 ScrollReveal().reveal('.btn-cv');
 
-ScrollReveal().reveal('.about', { delay: 1000 });
+ScrollReveal().reveal('#about', { delay: 1000 });
 
-ScrollReveal().reveal('.projects', { delay: 1000 });
+ScrollReveal().reveal('#projects', { delay: 1000 });
 
 ScrollReveal().reveal('.project', { delay: 1000 });
 
-ScrollReveal().reveal('.skills', { delay: 1000 });
+ScrollReveal().reveal('#skills', { delay: 1000 });
 
-ScrollReveal().reveal('.faq', { delay: 1000 });
+ScrollReveal().reveal('#faq', { delay: 1000 });
 
-ScrollReveal().reveal('.contact', { delay: 1000 });
+ScrollReveal().reveal('#contact', { delay: 1000 });
