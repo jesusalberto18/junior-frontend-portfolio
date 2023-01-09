bf656e7f90117d905ea016138ee74432b9c53b9e
Alberto <albertiae.digital@gmail.com>
2023-01-09 19:32:57 -0400

    Added more details to the smtp function

--- js/smtp.js
+++ js/smtp.js
@@ -1,11 +1,16 @@
 function senEmail() {
 	Email.send({
-	    SecureToken : "",
+	    SecureToken : "8da7e806-3c1d-4c57-992e-3d95ede64f91",
 	    To : 'albertiae.digital@gmail.com',
 	    From : document.getElementById("email").value,
-	    Subject : "Subject:", document.getElementById("subject").value,
-	    Body : "And this is the body"
+	    Subject : document.getElementById("subject").value + " is interested in your services.",
+	    Body : "Name: " + document.getElementById("name").value
+	    		+ "<br> Last Name: " + document.getElementById("lastname").value
+				+ "<br> Email: " + document.getElementById("email").value
+				+ "<br> Phone: " + document.getElementById("phone").value
+				+ "<br>"
+				+ "<br> Message: " + document.getElementById("message").value
 	}).then(
-	  message => alert(message)
+	  message => alert("Message sent")
 	);
 }
